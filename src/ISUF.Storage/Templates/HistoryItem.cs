bf37f340f8f2a2552653b5b00120f81e23e31282
Jan Rajnoha <janrajnoha@outlook.com>
2019-08-24 00:04:28 +0200

    v0 Update 40 -> Users and History Items

---
+++ src/ISUF.Storage/Templates/HistoryItem.cs
@@ -0,0 +1,36 @@
+﻿using ISUF.Base.Template;
+using ISUF.Storage.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISUF.Storage.Templates
+{
+    class HistoryItem : BaseItem
+    {
+        public int ItemID { get; set; }
+        public DbTypeOfChange TypeOfChange { get; set; }
+        public string ModuleType { get; set; }
+
+        public HistoryItem(int itemID, DbTypeOfChange typeOfChange, string moduleType)
+        {
+            ItemID = itemID;
+            TypeOfChange = typeOfChange;
+            ModuleType = moduleType;
+        }
+
+        public HistoryItem(HistoryItem historyItem) : base(historyItem)
+        {
+            ItemID = historyItem.ItemID;
+            TypeOfChange = historyItem.TypeOfChange;
+            ModuleType = historyItem.ModuleType;
+        }
+
+        public override object Clone()
+        {
+            return new HistoryItem(this);
+        }
+    }
+}
---
+++ src/ISUF.Storage/Templates/UserItem.cs
@@ -0,0 +1,41 @@
+﻿using ISUF.Base.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ISUF.Storage.Templates
+{
+    class UserItem : BaseItem
+    {
+        public string UserName { get; set; }
+        public byte[] Hash { get; set; }
+        public byte[] Salt { get; set; }
+
+        [XmlIgnore]
+        public string Password { get; set; } = string.Empty;
+
+        public UserItem(string userName, byte[] hash, byte[] salt)
+        {
+            UserName = userName;
+            Hash = hash;
+            Salt = salt;
+        }
+
+        public UserItem(UserItem userItem) : base(userItem)
+        {
+            UserName = userItem.UserName;
+            Hash = userItem.Hash;
+            Salt = userItem.Salt;
+            Password = userItem.Password;
+        }
+
+        public override object Clone()
+        {
+            return new UserItem(this);
+        }
+    }
+}
