592c78adbfebca043ddb76f171f92c09070e3cad
Jan Rajnoha <janrajnoha@outlook.com>
2019-08-25 21:57:35 +0200

    v1 Update 41 -> Users and History modules

---
+++ src/ISUF.Security/SecureSign.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISUF.Security
+{
+    public static class SecureSign
+    {
+        private static (byte[] Salt, byte[] Hash) GetHash(string pass, byte[] salt = null)
+        {
+            byte[] saltByte;
+
+            if (salt == null)
+            {
+                saltByte = new byte[8];
+                using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+                    rngCsp.GetBytes(saltByte);
+            }
+            else
+                saltByte = salt;
+
+            Rfc2898DeriveBytes hash = new Rfc2898DeriveBytes(pass, saltByte);
+            hash.Reset();
+            return (saltByte, hash.GetBytes(256));
+        }
+
+        public static bool Decrypt(byte[] salt, byte[] hash, string pass) => Enumerable.SequenceEqual(GetHash(pass, salt).Hash, hash);
+
+        public static (byte[] Salt, byte[] Hash) Encrypt(string pass) => GetHash(pass);
+    }
+}
---
+++ src/ISUF.Storage/Modules/UserModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISUF.Storage.Modules
+{
+    class UserModule : StorageModule
+    {
+        public UserModule(Type moduleItemType, Type itemManagerType, string moduleTableName = null) : base(moduleItemType, itemManagerType, moduleTableName)
+        {
+        }
+
+        public UserModule(Type moduleItemType, string moduleName, Type itemManagerType, string moduleTableName = null) : base(moduleItemType, moduleName, itemManagerType, moduleTableName)
+        {
+        }
+    }
+}
